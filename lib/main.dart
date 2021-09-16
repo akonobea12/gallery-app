dead50d2d681be72d7868b1135e0ff757a216ae9
akonobea12 <asantewaadorcas57@gmail.com>
2021-09-16 13:45:31 +0000

    added another card

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,13 @@ class HomePage extends StatelessWidget {
     'https://images.unsplash.com/photo-1593642702749-b7d2a804fbcf?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80'
   ];
 
+  List<String> _images2 = [
+    'https://images.unsplash.com/photo-1628191136272-08f5d3d9a6c0?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+    'https://images.unsplash.com/photo-1631730486784-5456119f69ae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=820&q=80',
+    'https://images.unsplash.com/photo-1631605858153-896f22f42f2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=794&q=80',
+    'https://images.unsplash.com/photo-1631715193954-68842ba0b843?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -119,7 +126,7 @@ class HomePage extends StatelessWidget {
                                     width: 5,
                                   ),
                                   Text(
-                                    '233',
+                                    '5.1M',
                                     style: TextStyle(fontSize: 14),
                                   ),
                                 ],
@@ -169,7 +176,113 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-            )
+            ),
+             Card(
+              color: Colors.white,
+              margin: EdgeInsets.fromLTRB(16, 10, 16, 10),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1631714222798-9b7ae46118b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2468&q=80'),
+                    ),
+                    title: Text(
+                      'Ransford',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 17),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          size: 14,
+                          color: Colors.blue,
+                        ),
+                        Text('Kasoa,Ghana')
+                      ],
+                    ),
+                  ),
+                  Image.network(
+                    'https://images.unsplash.com/photo-1631724533937-687e6e60aafe?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+                    height: 218,
+                    width: MediaQuery.of(context).size.width,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Material(
+                            color: Colors.grey.withOpacity(0.1),
+                            borderRadius: BorderRadius.circular(35),
+                            child: Padding(
+                              padding: EdgeInsets.all(16),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                  ),
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    '6.8M',
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          FlutterImageStack(
+                            imageList: _images2,
+                            showTotalCount: false,
+                            totalCount: 4,
+                            itemRadius: 35, //radius of each image
+                            itemCount:
+                                4, //maximum number of images to be shown in stack
+                            itemBorderWidth: 2, //border width around images
+                          ),
+                        ],
+                      ),
+                      Material(
+                        color: Colors.grey.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(35),
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.chat_bubble_fill,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                '6.5M',
+                                style: TextStyle(fontSize: 14),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
